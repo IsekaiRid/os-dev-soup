69d10a70064d6954322d526ad334656030bf6c24
IsekaiRid <mainrpg62@gmail.com>
2025-06-15 22:25:15 +0700

    test4

--- src/boot.asm
+++ src/boot.asm
@@ -1,8 +1,10 @@
+; src/boot.asm
 [bits 64]
 section .text
 global _start
+extern kernel_main        ; Fungsi C yang akan kita panggil
 
 _start:
-    cli
+    call kernel_main      ; Panggil fungsi dari C
     hlt
     jmp $
--- src/kernel.c
+++ src/kernel.c
@@ -1,12 +1,12 @@
 // src/kernel.c
-void _start() {
+void kernel_main() {
     const char* str = "Hello from kernel!";
     char* video = (char*)0xb8000;
 
     for (int i = 0; str[i] != '\0'; i++) {
-        video[i * 2] = str[i];        // Karakter
-        video[i * 2 + 1] = 0x07;      // Warna abu di latar hitam
+        video[i * 2] = str[i];
+        video[i * 2 + 1] = 0x07;
     }
 
-    while (1); // Gantung sistem
+    while (1);
 }
