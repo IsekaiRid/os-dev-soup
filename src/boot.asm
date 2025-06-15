99ed57ed1328c1b8ed5b73cd1cb6182fa0259e09
IsekaiRid <mainrpg62@gmail.com>
2025-06-15 22:20:22 +0700

    test2

--- src/boot.asm
+++ src/boot.asm
@@ -1,5 +1,12 @@
+[BITS 16]              ; Kita pakai real mode 16-bit
+[ORG 0x7C00]           ; BIOS akan load bootloader di alamat 0x7C00
+
 section .text
-global _start
-_start:
-    cli
-    hlt
+start:
+    cli                ; Clear interrupt
+    hlt                ; Halt CPU (berhenti)
+
+    jmp $              ; Loop di tempat agar tidak mengeksekusi sampah
+
+times 510 - ($ - $$) db 0 ; Isi padding hingga 510 byte
+dw 0xAA55               ; Signature boot sector di byte ke-511-512
