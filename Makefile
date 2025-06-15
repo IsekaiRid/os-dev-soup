a1032b59e8c638fa8fd4156bba48c5ffffc62c60
IsekaiRid <mainrpg62@gmail.com>
2025-06-15 22:44:09 +0700

    dev

--- Makefile
+++ Makefile
@@ -1,25 +1,5 @@
-CC=i686-elf-gcc
-LD=i686-elf-ld
-CFLAGS=-ffreestanding -O2 -Wall -Wextra
-LDFLAGS=-Tlink.ld
-
-all: iso
-
-kernel.o: src/kernel.c
-	$(CC) $(CFLAGS) -c $< -o $@
-
-boot.o: src/boot.asm
-	nasm -f elf32 $< -o $@
-
-kernel.bin: boot.o kernel.o
-	$(LD) $(LDFLAGS) -o $@ $^
-
-iso: kernel.bin
-	mkdir -p iso/boot/grub
-	cp kernel.bin iso/boot/kernel.bin
-	cp boot/grub/grub.cfg iso/boot/grub
-	grub-mkrescue -o myos.iso iso
+boot.img: boot.asm
+	nasm -f bin src/boot.asm -o boot.img
 
 clean:
-	rm -f *.o *.bin *.iso
-	rm -rf iso
+	rm -f boot.img
--- src/boot.asm
+++ src/boot.asm
@@ -1,16 +1,37 @@
-; boot.asm
-BITS 32
-section .multiboot
-align 4
-    dd 0x1BADB002              ; magic
-    dd 0x00                    ; flags
-    dd - (0x1BADB002 + 0x00)   ; checksum
+; boot.asm - boot sector dengan pesan Hello OS
+[org 0x7c00]
+bits 16
 
-section .text
-global _start
-extern kernel_main
+start:
+    ; clear screen
+    mov ah, 0x0
+    mov al, 0x3
+    int 0x10
 
-_start:
-    call kernel_main
-hang:
-    jmp hang
+    ; tulis string ke VGA text mode
+    mov si, message
+    mov di, 0xb800
+    mov ax, 0xB800
+    mov es, ax
+    mov bx, 0        ; kolom awal
+
+.loop:
+    lodsb
+    cmp al, 0
+    je .hang
+    mov [es:bx], al
+    inc bx
+    mov byte [es:bx], 0x07 ; warna: abu putih
+    inc bx
+    jmp .loop
+
+.hang:
+    cli
+    hlt
+    jmp $
+
+message db "Hello OS", 0
+
+; isi sisa jadi 512 byte
+times 510-($-$$) db 0
+dw 0xAA55
