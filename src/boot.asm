24ddf4b38b2d87de605580234f52d3cb153010c9
IsekaiRid <mainrpg62@gmail.com>
2025-06-15 22:28:44 +0700

    test5

--- src/boot.asm
+++ src/boot.asm
@@ -1,10 +1,13 @@
 ; src/boot.asm
-[bits 64]
+BITS 32
+section .multiboot
+align 4
+    dd 0x1BADB002              ; magic number
+    dd 0x00                    ; flags
+    dd - (0x1BADB002 + 0x00)   ; checksum = -(magic + flags)
+
 section .text
 global _start
-extern kernel_main        ; Fungsi C yang akan kita panggil
-
 _start:
-    call kernel_main      ; Panggil fungsi dari C
+    cli
     hlt
-    jmp $
